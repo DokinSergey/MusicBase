cb954da00c1cc3b3400fe6edf2cfc67dc3ae7cf4
Dokin Sergey <dokin.serg@gmail.com>
2026-07-12 12:47:32 +0300

    2026-07-12

---
+++ Date_Inser_track.sql
@@ -0,0 +1,5 @@
+﻿-- Дата загрузки трека 2026-06-07
+-- Update PlaylistUpdatable Set comment = substr(file_created,0,11)
+select substr(file_created,0,11),comment,* from PlaylistUpdatable 
+Where playlist_name = 'Dokin' and comment is null
+-- Where playlist_name = 'Dokin' and trim(comment) != substr(file_created,0,11) 
